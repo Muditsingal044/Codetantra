f45d8e590af20f1f7421cf501c282033eeb82325
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-08-12 15:31:12 +0530

    Update 3.py

--- Competitive Coding - 1/1. Problems on Arrays/3.py
+++ Competitive Coding - 1/1. Problems on Arrays/3.py
@@ -1,1 +1,9 @@
-sumHighestAndLowestElements
+import sys
+
+arr = [int(x) for x in sys.argv[1].split(',')]
+
+def middleElementOfArray(arr):
+  l = len(arr) - 1
+  return arr[l//2]
+
+print(middleElementOfArray(arr))
