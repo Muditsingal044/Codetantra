dcfebaa420895f0cdda7dcb4bddd9d94fdc43544
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-08-12 18:19:52 +0530

    Update 5.py

--- Competitive Coding - 1/3.Probelms on Mathematics/5.py
+++ Competitive Coding - 1/3.Probelms on Mathematics/5.py
@@ -2,7 +2,7 @@ import sys
 
 N = int(sys.argv[1])
 L = int(sys.argv[2])
-R = int(sys.argv[2])
+R = int(sys.argv[3])
 
 def sumBetween(N, L, R):
   a = [i for i in range(0,N+1,2)] + [i for i in range(1,N+2,2)]
