b9b8888147bae92cec06defb066c3c45e9c6be46
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-08-12 16:58:56 +0530

    Create 3.py

---
+++ Competitive Coding - 1/2. Problems of Strings/3.py
@@ -0,0 +1,14 @@
+s = sys.argv[1]
+l = int(sys.argv[2])
+
+def isEqualCharacters(s, l):
+  s = s[:l]
+  rev = s[::-1]
+  rev = rev[:l]
+
+if s==rev:
+  return True
+else:
+  return False
+
+print("true" if isEqualCharacters(s, l) else "false");
