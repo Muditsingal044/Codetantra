5ced2fd06237b9ff94cd001031f8a416cf1a435c
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-08-12 17:12:46 +0530

    Update 6.py

--- Competitive Coding - 1/2. Problems of Strings/6.py
+++ Competitive Coding - 1/2. Problems of Strings/6.py
@@ -1,1 +1,20 @@
+import sys
 
+S = sys.argv[1]
+
+ch = 'QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm'
+
+def reverselAlphas(S):
+  l=[]
+  for i in S:
+    if i in ch:
+      l.append()
+  s=''
+  for i in S:
+    if i in ch:
+      s+=l.pop()
+    else:
+      s+=i
+  return s
+  
+print(reverselAlphas(S));
