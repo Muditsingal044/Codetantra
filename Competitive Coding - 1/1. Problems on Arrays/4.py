c032dce573831ca0e5894377b2183a44d67a3e19
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-08-12 15:36:46 +0530

    Update 4.py

--- Competitive Coding - 1/1. Problems on Arrays/4.py
+++ Competitive Coding - 1/1. Problems on Arrays/4.py
@@ -1,1 +1,16 @@
+import sys
 
+arr = [int(x) for x in sys.argv[1].split(',')]
+
+def maxSumContiguousSubArray(arr):
+  sum1 = 0
+  ans = 0
+  for i in range(len(arr)):
+    sum1+= arr[i]
+    if sum1<0:
+      sum1 = 0
+    elif ans < sum1:
+      ans = sum1
+  return ans 
+
+print(maxSumContiguousSubArray(arr))
