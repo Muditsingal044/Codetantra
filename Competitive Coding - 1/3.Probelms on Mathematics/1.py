788c31212ed25eca784e1b1edcdb2e5eea0732dc
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-08-12 17:59:10 +0530

    Update 1.py

--- Competitive Coding - 1/3.Probelms on Mathematics/1.py
+++ Competitive Coding - 1/3.Probelms on Mathematics/1.py
@@ -1,1 +1,28 @@
+import sys
 
+n1 = int(sys.argv[1])
+n2 = int(sys.argv[2])
+
+def isprime(a):
+  if a<2:
+    return False
+  if a==2:
+    return True
+  for i in range(2,a):
+    if a%i==0:
+      return False
+  else:
+    return True
+  
+def sumOfPrimes(n1, n2):
+  l=[]
+  if n2<n1:
+    n3=n1
+    n1=n2
+    n2=n3
+  for i in range(n1,n2):
+    if isprime(i):
+      l.append(i)
+  return sum(l)
+
+print(sumOfPrimes(n1, n2))
