6533877d249ee9c23068d770e68d8675bbdccb11
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-08-21 16:13:26 +0530

    Update 3.py

--- Competitive Coding - 1/2. Problems of Strings/3.py
+++ Competitive Coding - 1/2. Problems of Strings/3.py
@@ -3,13 +3,12 @@ s = sys.argv[1]
 l = int(sys.argv[2])
 
 def isEqualCharacters(s, l):
-  s = s[:l]
+  stt = s[:l]
   rev = s[::-1]
   rev = rev[:l]
-
-if s==rev:
-  return True
-else:
-  return False
+  if stt==rev:
+    return True
+  else:
+    return False
 
 print("true" if isEqualCharacters(s, l) else "false");
