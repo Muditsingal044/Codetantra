b30f0c8ea9554a2f8643211a67e908a6c7010ac5
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-08-31 08:39:07 +0530

    Update 4.py

--- Competitive Coding - 1/2. Problems of Strings/4.py
+++ Competitive Coding - 1/2. Problems of Strings/4.py
@@ -4,7 +4,7 @@ s = sys.argv[1]
 
 def isSuper(s):
   keys = [chr(x) for x in range(97,123)]
-  values = [x for i in range(1,27)]
+  values = [x for x in range(1,27)]
   d = {k:v for k,v in zip(keys,values)}
   for i in s:
     if s.count(i)!=d[i]:
