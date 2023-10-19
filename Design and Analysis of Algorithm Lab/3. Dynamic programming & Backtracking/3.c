e7586d8f5741cbd51f8f82a2cb7d9ba1f155656b
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-10-19 13:11:23 +0530

    Update 3.c

--- Design and Analysis of Algorithm Lab/3. Dynamic programming & Backtracking/3.c
+++ Design and Analysis of Algorithm Lab/3. Dynamic programming & Backtracking/3.c
@@ -12,13 +12,13 @@ int knapsack(int W, int v[], int w[], int n){
 void main(){
 	int n, W;
 	printf("Enter number of items:");
-	scanf("%d", & n);
+	scanf("%d", &n);
 	int v[n], w[n];
 	printf("Enter value and weight of items:");
 	for (int i = 0; i < n; i++){
-		scanf("%d %d", & v[i], & w[i]);
+		scanf("%d %d", &v[i], &w[i]);
 	}
 	printf("Enter size of knapsack:");
-	scanf("%d", & W);
+	scanf("%d", &W);
 	printf("Maximum value in 0/1 knapsack :%d", knapsack(W, v, w, n));
 }
