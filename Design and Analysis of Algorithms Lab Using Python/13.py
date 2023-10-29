a16eb5c6e291386a892c741769161f47e72c14f1
Mudit Singal <117852494+Muditsingal044@users.noreply.github.com>
2023-10-29 15:08:50 +0530

    Create 13.py

---
+++ Design and Analysis of Algorithms Lab Using Python/13.py
@@ -0,0 +1,24 @@
+
+n = int(input())
+m = int(input())
+
+graph = [[] for i in range(n)]
+for i in range(m):
+	u, v = map(int, input().split())
+	graph[u].append(v)
+	        
+start_vertex = int(input())
+	        
+def bfs(graph, start_vertex):
+	visited = [False] * len(graph)
+	queue = [start_vertex]
+	        	            
+	while queue:
+		vertex = queue.pop(0)
+		if not visited[vertex]:
+			print(vertex, end=' ')
+			visited[vertex] = True
+			for neighbor in graph[vertex]:
+				if not visited[neighbor]:
+					queue.append(neighbor)
+bfs(graph, start_vertex)
